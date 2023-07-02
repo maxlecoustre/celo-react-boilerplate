4ca03bdb9955f73f5ff217561ee8c5b8b7fa8701
LECOUSTRE Maxime <MLECOUSTRE@nexity.fr>
2023-07-02 21:24:08 +0200

    fix openzeppelin import

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/tokens/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/tokens/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
