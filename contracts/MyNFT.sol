7154e59a809ef856ba14e5bbd7284b571929eb2b
LECOUSTRE Maxime <MLECOUSTRE@nexity.fr>
2023-07-02 21:27:07 +0200

    fix openzeppelin import 2

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERX721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
