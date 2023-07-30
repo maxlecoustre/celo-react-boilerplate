c051e2193a7321c91485b9f3a04f3dfe646f280c
LECOUSTRE Maxime <MLECOUSTRE@nexity.fr>
2023-07-30 11:41:25 +0200

    fix typo

--- src/hooks/useMinterContract.js
+++ src/hooks/useMinterContract.js
@@ -1,6 +1,6 @@
 import { useContract } from "./useContract";
 import MyNFTAbi from "../contracts/MyNFT.json";
-import MyNFTContractAddress from "../contracts/MyNFTaddress.json";
+import MyNFTContractAddress from "../contracts/MyNFTAddress.json";
 
 export const useMinterContract = () =>
     useContract(MyNFTAbi.abi, MyNFTContractAddress.MyNFT);
